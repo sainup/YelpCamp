24244066c4c5b48e071dc9584b945a14659a4a02
Sainup Karki <sainup.karki999@gmail.com>
2020-11-19 16:45:03 +0100

    Fixing campgrounds model

--- models/campground.js
+++ models/campground.js
@@ -5,54 +5,64 @@ const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-    url : String,
-    filename :String
+    url: String,
+    filename: String
 });
 
 
 
-ImageSchema.virtual('thumbnail').get(function (){
-   return this.url.replace('/upload','/upload/w_200');
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
 });
 
-const opts = { toJSON : {virtuals : true}};
+const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
     title: String,
-    images:  [ImageSchema],
-    geometry : {
-        type : {
-            type : String,
-            enum : ['Point'],
-            required : true 
-            
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+
         },
-        coordinates : {
-            type : [Number],
-            required : true
-            
+        coordinates: {
+            type: [Number],
+            required: true
+
         }
     },
-    price : Number,
-    description : String,
-    location : String,
-    author:{
-        type : Schema.Types.ObjectId,
-        ref : 'User'
+    price: Number,
+    description: String,
+    location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     },
-    reviews : [
+    reviews: [
         {
-            type : Schema.Types.ObjectId,
-            ref : 'Review'
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
         }
     ]
 }, opts);
-CampgroundSchema.virtual('properties.popUpMarkUp').get(function (){
-    return `<img src=" ${this.images[0].thumbnail}" styles = "width : 100%;">
-            <a href="/campgrounds/${this._id}">${this.title}</a>
-            <p>${this.description}</p>
+CampgroundSchema.virtual('properties.popUpMarkUp').get(function () {
+
+    if (this.images.length) {
+
+        return `<img src=" ${this.images[0].thumbnail}" styles = "width : 100%;">
+        <a href="/campgrounds/${this._id}">${this.title}</a>
+        <p>${this.description}</p>
+
+`
+    } else {
+        return `<img src=" "https://www.danob.com.bd/wp-content/uploads/2020/06/nophotofound.png" styles = "width : 100%;">
+        <a href="/campgrounds/${this._id}">${this.title}</a>
+        <p>${this.description}</p> `
+    }
+
     
-    `
  });
 
  
